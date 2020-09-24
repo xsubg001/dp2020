33612f044fb4c6e31c06f0a4a690739256c0400a
Radim Cimoradsky <racimora@microsoft.com>
2020-09-24 19:26:12 +0200

    added Inheritance feature

--- ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -24,18 +24,18 @@ namespace ContosoUniversity.Data
         public DbSet<Instructor> Instructors { get; set; }
         public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
         public DbSet<CourseAssignment> CourseAssignments { get; set; }
-
+        public DbSet<Person> People { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().ToTable("Course");
             modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Student>().ToTable("Student");
+            //modelBuilder.Entity<Student>().ToTable("Student");
             modelBuilder.Entity<Department>().ToTable("Department");
-            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            //modelBuilder.Entity<Instructor>().ToTable("Instructor");            
             modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
             modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
-
+            modelBuilder.Entity<Person>().ToTable("Person");
             modelBuilder.Entity<CourseAssignment>()
                 .HasKey(c => new { c.CourseID, c.InstructorID });
 
@@ -43,7 +43,6 @@ namespace ContosoUniversity.Data
             //   .HasOne(d => d.Administrator)
             //   .WithMany()
             //   .OnDelete(DeleteBehavior.Restrict);
-
         }
 
         public void ConfigureServices(IServiceCollection services)
