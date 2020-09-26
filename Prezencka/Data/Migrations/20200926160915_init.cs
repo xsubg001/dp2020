212b6d9ff1cd133cc3a553cfeac818b754dfde81
xsubg001 <gabika.subova@gmail.com>
2020-09-26 18:11:32 +0200

    Prezencka initial migration

---
+++ Prezencka/Data/Migrations/20200926160915_init.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prezencka.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
