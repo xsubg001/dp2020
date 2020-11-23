f7ba0fd879e27b0f428b7a2d7c6bbdca38dfb3de
xsubg001 <gabika.subova@gmail.com>
2020-11-23 23:36:38 +0100

    adding some skeleton for role and user management

---
+++ Dochazka/Models/ManageUserRolesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dochazka.Models
+{
+    /// <summary>
+    /// Model which helps to manage user roles in UserRolesController
+    /// </summary>
+    public class ManageUserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ Dochazka/Models/UserRolesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dochazka.Models
+{
+    /// <summary>
+    /// Helps to generate Index view in UserRolesController
+    /// </summary>
+    public class UserRolesViewModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+
+    }
+}
