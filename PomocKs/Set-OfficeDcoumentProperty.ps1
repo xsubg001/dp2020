4a93ed4c2b951aa6cfc682cd51c8f5422cb98c2b
xsubg001 <gabika.subova@gmail.com>
2021-01-03 14:47:18 +0100

    update

--- PomocKs/Set-OfficeDcoumentProperty.ps1
+++ PomocKs/Set-OfficeDcoumentProperty.ps1
@@ -1,21 +1,23 @@
 <#
-By default, C:\Temp folder is scanned for *.doc and *.docx and those are opened and saved with actual system date.
-If option "-antidateEnabled" is on, it must be run in admin console, because Set-Date requires Admin permissions. It may also require:
+This is used to modify Last Update timestamp for Excel and Word documents. It is using Set-Date function to adjust temporarily computer system date.
+Then, it reopens and saves the selected office documents.
+By default, C:\Temp folder is scanned for *.doc, *.docx, *.xls, *.xlsx and those are opened and saved with actual system date.
+If option "-antidateEnabled" is on, it must be run in Powershell Admin console, because Set-Date function requires Admin permissions. It may also require:
 "Set-ExecutionPolicy -ExecutionPolicy Unrestricted" to be able to run this script
 
 Run as following: 
     1. cd <folder-containing-this-script>
     2a. Either in normal Powershell console/ISE window:
-    .\Set-MSWordProperty.ps1 -path C:\MyDocuments
+    .\Set-OfficeDocumentProperty.ps1 -path C:\MyDocuments
 
     2b. Or in admin/elevated Powershell console/ISE window with "antidateEnabled" parameter set:
-    .\Set-MSWordProperty.ps1 -antidateEnabled -newOldDate 2018-06-18 
-    .\Set-MSWordProperty.ps1 -antidateEnabled -newOldDate 2018-06-18 -path C:\MyDocuments
+    .\Set-OfficeDocumentProperty.ps1 -antidateEnabled -newOldDate 2018-06-18 
+    .\Set-OfficeDocumentProperty.ps1 -antidateEnabled -newOldDate 2018-06-18 -path C:\MyDocuments
 #>
 
 Param(
     $path = "C:\temp",
-    [array]$include = @("*.doc", "*.docx"),
+    [array]$include = @("*.doc", "*.docx", "*.xls", "*.xlsx"),
 
     [Parameter(ParameterSetName = 'Antidate')]
     [DateTime]$newOldDate = "2016-06-18", # YYYY-MM-DD
@@ -25,7 +27,6 @@ Param(
 )
 
 if ($antidateEnabled) {        
-
     $originalDate = Get-Date
     $maxNewOldDate = [DateTime]"$($newOldDate.Year)-$($newOldDate.Month)-$($newOldDate.Day) 23:59:59"
     $randomTicks = Get-Random -Minimum $newOldDate.Ticks -Maximum $maxNewOldDate.Ticks
@@ -36,17 +37,25 @@ if ($antidateEnabled) {
     set-date $newRandomDate | Out-Null
 }
 
-
 $docs = Get-childitem -path $Path -Recurse -Include $include 
 
 #Create Word application
-$wordApplication = New-Object -ComObject word.application
+$wordApplication = New-Object -ComObject Word.application
+$excelApplication = New-Object -ComObject Excel.application
 
 Foreach ($doc in $docs) {
     write-host "`r`nUpdating file: $($doc.FullName)"
-
-    #Get reference to word doc
-    $document = $wordApplication.documents.open($doc.FullName);
+    
+    if ($doc.Name -match ".xls") {
+        $application = $excelApplication
+        #Get reference to Excel doc
+    $document = $application.WorkBooks.open($doc.FullName);
+    }
+    else {
+        $application = $wordApplication
+        #Get reference to word doc
+        $document = $application.documents.open($doc.FullName);
+    }    
 
     #set up binding flags for custom properties
     $binding = "System.Reflection.BindingFlags" -as [type];        
@@ -55,7 +64,7 @@ Foreach ($doc in $docs) {
     [Array]$propertyName = "Comments"
     # [Array]$propertyName = "Creation Date"
     # [Array]$propertyName = "Last Save Time"
-    [Array]$propertyValue = (Get-Date).Ticks
+    [Array]$propertyValue = (Get-Date -Date $newOldDate).Ticks
     
     #Get property value
     $myProperty = [System.__ComObject].InvokeMember("Item", $binding::GetProperty, $null, $customProperties, $propertyName)
@@ -68,17 +77,14 @@ Foreach ($doc in $docs) {
     #Get property value
     $myPropertyValue = [System.__ComObject].InvokeMember("value", $binding::GetProperty, $null, $myProperty, $null);
     Write-Host "New value: $propertyName, $myPropertyValue"
-
-    #Update all fields
-    $document.Fields.Update() | Out-Null
     
-    #save and close document
-    #required for O365
+    # Close the workbook with save forced
     $document.Saved = $false;
-    $document.save();    
+    $document.Close($true)  
 }
 
-$wordApplication.Quit();
+$wordApplication.Quit()
+$excelApplication.Quit()
 Start-Sleep -Seconds 1
 
 if ($antidateEnabled) {
