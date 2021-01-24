84171deb7dff35f5dd676ac6324144f8f890ab56
xsubg001 <gabika.subova@gmail.com>
2021-01-25 00:31:02 +0100

    update

--- Dochazka/scripts/PublishCommands.ps1
+++ Dochazka/scripts/PublishCommands.ps1
@@ -3,23 +3,10 @@ $location = "West Europe"
 $serverName = "dp2020gacisqlwe"
 $dbName = "dochazkaDB"
 $myIPaddress = "185.186.249.40"
-$sqlServerAdminUser = "gabika"
+$sqlServerAdminUser = Read-Host -Prompt "Enter SQL server admin userName"
 $password = Read-Host -Prompt "Enter SQL server admin password"
 $sqlServerAdminPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 $sqlAdminCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sqlServerAdminUser, $sqlServerAdminPassword
-
-
-
-
-
-# az login
-# az account show
-# az group create --name dp2020 --location $location
-# az sql server create --name dp2020we --resource-group dp2020 --location $location --admin-user gabika --admin-password "tbd"
-# az sql server firewall-rule create --resource-group dp2020 --server dp2020we --name AllowAzureIps --start-ip-address 0.0.0.0 --end-ip-address 0.0.0.0
-# az sql server firewall-rule create --name AllowMyLocalDesktop --server dp2020we --resource-group dp2020 --start-ip-address=185.186.249.40 --end-ip-address=185.186.249.40
-# az sql db create --resource-group dp2020 --server dp2020we --name diplomka --service-objective Basic
-# az sql db show-connection-string --client ado.net --server $serverName --name $dbName
 
 Connect-AzAccount -Tenant 1c23c01a-0a11-4849-a1bd-eddfe415c6d1 -Subscription bdd5ddce-ffd0-49dd-961a-d74ab44a262e
 New-AzResourceGroup -Name $rgName -Location $location
@@ -31,13 +18,12 @@ Get-AzSqlDatabase -ResourceGroupName $rgName -ServerName $serverName -DatabaseNa
 
 $connectionString = "Server=tcp:dp2020gacisqlwe.database.windows.net,1433;Database=dochazkaDB;User ID=$sqlServerAdminUser;Password=$password;Encrypt=true;Connection Timeout=30;"
 
-
-
 exit;
 
 rm -r Migrations
 Add-Migration initialcreate
 $env:ConnectionStrings:DefaultConnection = $connectionString
+$env:ConnectionStrings:DefaultConnection = ""
 Update-Database
 
 
