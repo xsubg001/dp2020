557cd20453f93f955aff34a95d918c7669fa66c8
xsubg001 <59803681+xsubg001@users.noreply.github.com>
2021-03-21 00:07:45 +0100

    Gabika/develop (#9)
    * kosmeticke zmeny ve Views
    
    * cisteni zdrojoveho kodu
    
    * optimalizace ZK
    
    * zmeny kvuli aktualizaci modelu
    
    * aktualizace TeamsController
    
    * dalsi drobne zmeny TeamsController
    
    * aktualizace Controlleru podle textu DP
    
    * cisteni a optimalizace kodu
    
    * drobny refactoring v prubehu textu DP
    
    * hotovy popis testu
    
    * zmeny behem textace deploymentu do Azure
    
    * pridano logovani
    
    * oznaceni develop branch
    
    * log
    
    * u

--- Dochazka/Program.cs
+++ Dochazka/Program.cs
@@ -20,6 +20,7 @@ namespace Dochazka
             {
                 var services = scope.ServiceProvider;
                 var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogInformation("DochazkaGabika: Starting the application.");
 
                 try
                 {
@@ -30,13 +31,15 @@ namespace Dochazka
                     var config = host.Services.GetRequiredService<IConfiguration>();
                     
                     // test configuration service:
-                    logger.LogWarning("TestKey: {TestValue}", config["TestKey"]);
+                    logger.LogInformation("TestKey: {TestValue}", config["TestKey"]);
 
                     // Set password with the Secret Manager tool, dotnet user-secrets set SeedUserPW <pw>                    
                     var testUserPw = config["SeedUserPW"];
-                    
+                    logger.LogInformation("Length SeedUserPW: {0}", testUserPw.Length);
+
                     // Seed database
-                    SeedData.Initialize(services, testUserPw).Wait();                    
+                    SeedData.Initialize(services, testUserPw, logger).Wait();
+                    logger.LogInformation("Completed seeding the DB.");
                 }
                 catch (Exception ex)
                 {                    
