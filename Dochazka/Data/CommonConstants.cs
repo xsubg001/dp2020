b981c5e0071d1976cea799f0a38ba36c764770a5
xsubg001 <gabika.subova@gmail.com>
2021-03-28 11:18:47 +0200

    navyseni velikosti stranky v aplikaci

--- Dochazka/Data/CommonConstants.cs
+++ Dochazka/Data/CommonConstants.cs
@@ -3,7 +3,7 @@
     public static class CommonConstants
     {
         public const string DEFAULT_TEAM = "DefaultTeam";
-        public const int PAGE_SIZE = 5;
+        public const int PAGE_SIZE = 10;
         public const string TEAM_MANAGER_USERNAME = "Team Manager Username";
     }
 }
